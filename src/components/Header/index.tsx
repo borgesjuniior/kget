26cf90f115490440fc5dadc68addf765a259453f
Júnior <valdecyborgesjr@hotmail.com>
2021-03-01 23:10:49 -0300

    Feat:Add GlobalStyle and Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';  
+import { Container } from './styles';
+
+export const Header: React.FC = () => {
+  return (
+    <Container>
+      <h1>Explore perfis no Instagram</h1>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 5rem;
+  background:#222222;
+  color: var(--white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
